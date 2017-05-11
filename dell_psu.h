bbfe2e240f07d3fea7b67a8411e8ff0d32100219
timothyjager <timothyjager@users.noreply.github.com>
2017-05-11 16:15:16 -0500

    initial commit

---
+++ dell_psu.h
@@ -0,0 +1,46 @@
+/*
+Copyright (c) 2017, Tim Jager
+This library reads data from a Dell laptop power supply using 1 wire protocol
+MIT license, check license.txt for more information
+*/
+
+#ifndef dell_psu_h
+#define dell_psu_h
+
+//this library requires the OneWire library (https://github.com/PaulStoffregen/OneWire)
+#include <OneWire.h>
+
+//we try to read up to x bytes from the device (16 bytes is the minimum to extract the power supply wattage, voltage, & amps
+#define DELL_PSU_BYTES_TO_READ 16
+
+class DellPSU
+{
+  private:
+      uint16_t _watts = 0;
+      uint16_t _millivolts = 0;
+      uint16_t _milliamps = 0;
+      OneWire _onewire;
+
+  public:
+    DellPSU( uint8_t pin);
+
+    //Determine if a device is present, return true on success
+    boolean psu_detected(void);
+
+    //Read the data stream from the psu, return true on success
+    //String read_data(void);
+    boolean read_data(void);
+    
+    //get the watts
+    uint16_t watts(void);
+    
+    //get the millivolts
+    uint16_t millivolts(void);
+    
+    //get the milliamps
+    uint16_t milliamps(void);   
+};
+
+#endif
+
+
---
+++ examples/read_data/read_data.ino
@@ -0,0 +1,53 @@
+/*
+Reads the data from a dell charger using OneWire and parses the wattage, voltage, and current
+
+Connect the center pin of the Dell power supply to the desired Arduino I/O pin
+Add a 4.7K pull-up resistor between the tiny center pin of the Dell power supply and the Arduino +5V rail 
+Connect the ground pin of the Dell power supply to the Arduino GND
+Do not connect the 19.5V pin of the Dell power supply to the Arduino!!!! 
+*/
+
+#include "dell_psu.h"
+
+DellPSU dell(6);   //specify the desired Arduino pin number  
+  
+void setup() {
+  Serial.begin (115200);
+  while(!Serial); 
+}
+
+void loop() {
+  if (dell.read_data()==true)
+  {
+    Serial.println("");
+    Serial.print(dell.watts());
+    Serial.print("W "); 
+    Serial.print(dell.millivolts());
+    Serial.print("mv ");
+    Serial.print(dell.milliamps());
+    Serial.println("mA "); 
+  }
+  else
+  {
+    Serial.print(".");
+  }
+  delay(1000);
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
